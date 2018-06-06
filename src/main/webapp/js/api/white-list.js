d14a06c12ad144acb18badd1186edddd7b071cf2
StruggleYang <yq1724555319@gmail.com>
2018-06-06 22:34:11 +0800

    白名单添加优化

--- src/main/webapp/js/api/white-list.js
+++ src/main/webapp/js/api/white-list.js
@@ -10,11 +10,11 @@ InitWhiteList = function () {
     var url = basePath + "/api/sysWhiteList";
     var whiteContext = "";
     $.get(url,function (res) {
-        console.log(res);
         for(var i = 0;i<res.context.length;i++){
             whiteContext += '<li class="list-group-item">'+res.context[i]+'</li>'
         }
         $("#white-list-group").html(whiteContext);
+        $("#white-list-group").addClass("animated fadeIn");
     },"json")
 };
 
@@ -27,7 +27,7 @@ addWhiteItem = function () {
     $.post(url,{
         service:text
     },function (res) {
-        console.log(res);
         layer.msg(res.context);
+        InitWhiteList();
     },"json")
 };
