075daf4dc095954150eacf6fae991b828623331b
StruggleYang <1724555319@qq.com>
2018-06-07 20:11:39 +0800

    apikey

---
+++ src/main/webapp/js/api/apikey.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+    InitApiTable();
+});
+
+function InitApiTable() {
+    //记录页面bootstrap-table全局变量$table，方便应用
+    var queryUrl = basePath + '/api/apikey';
+    var rows = 10;
+    $table = $('#apikey-table').bootstrapTable({
+        url: queryUrl,                      //请求后台的URL（*）
+        method: 'GET',                      //请求方式（*）
+        responseHandler: function (res) {     //格式化返回数据
+            return {
+                total: res.context.totalElements,
+                rows: res.context.content
+            };
+        },
+        //toolbar: '#toolbar',              //工具按钮用哪个容器
+        striped: true,                      //是否显示行间隔色
+        cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+        pagination: true,                   //是否显示分页（*）
+        sortable: true,                     //是否启用排序
+        sortOrder: "desc",                   //排序方式
+        sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
+        pageNumber: 1,                      //初始化加载第一页，默认第一页,并记录
+        pageSize: rows,                     //每页的记录行数（*）
+        pageList: [5, 10, 15, 20],        //可供选择的每页的行数（*）
+        search: true,                      //是否显示表格搜索
+        strictSearch: false,                 //设置为 true启用全匹配搜索，否则为模糊搜索。
+        showColumns: true,                  //是否显示所有的列（选择显示的列）
+        showRefresh: false,                  //是否显示刷新按钮
+        minimumCountColumns: 2,             //最少允许的列数
+        clickToSelect: false,                //是否启用点击选中行
+        //height: 900,                      //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+        uniqueId: "id",                     //每一行的唯一标识，一般为主键列
+        showToggle: false,                   //是否显示详细视图和列表视图的切换按钮
+        cardView: false,                    //是否显示详细视图
+        detailView: false,                  //是否显示父子表
+        //得到查询的参数
+        queryParams: function (params) {
+            //这里的键的名字和控制器的变量名必须一致，这边改动，控制器也需要改成一样的
+            return {
+                keyword: params.search,
+                rows: params.limit,                         //页面大小
+                page: (params.offset / params.limit) + 1,   //页码
+                sort: params.sort,      //排序列名
+                sortOrder: params.order //排位命令（desc，asc）
+            };
+        },
+        columns: [{
+            checkbox: false,
+            visible: false,
+            formatter: numberFormatter//是否显示复选框
+        }, {
+            field: 'id',
+            title: '#'
+
+        }, {
+            field: 'ApiKey',
+            title: 'ApiKey',
+            sortable: true
+        }, {
+            field: 'password',
+            title: '密码'
+        }, {
+            field: 'biz',
+            title: '所属业务',
+            sortable: true,
+            align: 'center',
+            valign: 'middle'
+        }, {
+            field: 'ips',
+            title: 'IP规则'
+        }, {
+            field: 'notes',
+            title: '备注',
+            sortable: true
+        }, {
+            field: 'createdAt',
+            title: '添加时间',
+            sortable: true
+        }, {
+            field: 'updatedAt',
+            title: '修改时间',
+            sortable: true
+        }, {
+            field: 'id',
+            title: '操作',
+            width: 160,
+            align: 'center',
+            valign: 'middle'
+        }],
+        onLoadSuccess: function () {
+        },
+        onLoadError: function () {
+            layer.msg("数据加载失败");
+        },
+        // 双击行事件
+        onDblClickRow: function (row) {
+            var id = row.id;
+            viewOrEditByID(id, 'view');
+        }
+    });
+}
--- src/main/webapp/views/page/config-apikey.jsp
+++ src/main/webapp/views/page/config-apikey.jsp
@@ -9,6 +9,7 @@
 <html>
 <head>
     <jsp:include page="../core/resource.jsp"/>
+    <script src=""></script>
 </head>
 <body>
 <jsp:include page="../core/header.jsp"/>
@@ -27,42 +28,10 @@
             </div>
         </div>
 
-        <div class="panel-context">
-            <table class="table">
-                <thead>
-                <tr>
-                    <td>序号</td>
-                    <td>ApiKey</td>
-                    <td>密码</td>
-                    <td>所属业务</td>
-                    <td>IP规则</td>
-                    <td>备注</td>
-                    <td>最后修改时间</td>
-                    <td>操作</td>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>1223</td>
-                    <td>12312312</td>
-                    <td>wms</td>
-                    <td>供应链</td>
-                    <td>127.0.0.1</td>
-                    <td>xx年</td>
-                    <td>
-                        <span class="link-button-table">
-                            <%--如果未发布--%>
-                            <a href="#">分配</a>
-                            <%--如果已发布--%>
-                            <a href="#">修改</a>
-                            <a href="#">删除</a>
-                        </span>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
+        <table id="apikey-table">
+
+        </table>
+
     </div>
 </div>
 </body>
