63eacc34503172cdb7fb7cc553462cca9bb68a35
StruggleYang <1724555319@qq.com>
2018-06-13 09:52:46 +0800

    sql

--- sql/config_server_db.sql
+++ sql/config_server_db.sql
@@ -1,7 +1,8 @@
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
+CREATE SCHEMA config_server_db;
+USE config_server_db;
 -- ----------------------------
 --  Table structure for `api_key_info`
 -- ----------------------------
