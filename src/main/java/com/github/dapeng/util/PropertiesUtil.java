d3403848e9399ec9bedfd22c4b6015eec1ab1068
huyongjie <2573105190@qq.com>
2018-06-15 16:40:12 +0800

    update

--- src/main/java/com/github/dapeng/util/PropertiesUtil.java
+++ src/main/java/com/github/dapeng/util/PropertiesUtil.java
@@ -1,5 +1,8 @@
 package com.github.dapeng.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.util.Objects;
 import java.util.Properties;
@@ -11,10 +14,12 @@ import java.util.Properties;
  * @Created 2018/6/13 11:59
  */
 public class PropertiesUtil {
+    private static final Logger logger = LoggerFactory.getLogger(PropertiesUtil.class);
     public static Properties properties;
 
     public static void loadProperties() {
-        System.out.println("start load properties...");
+        System.out.println("");
+        logger.info("---------start load properties...-----------");
         properties = new Properties();
         try {
             // properties.load(in);
@@ -26,7 +31,7 @@ public class PropertiesUtil {
 
 
     public static String getProperty(String key, String defaultValue) {
-        return Objects.isNull(properties) ||  Objects.isNull(properties.get(key)) ? defaultValue : (String) properties.get(key);
+        return Objects.isNull(properties) || Objects.isNull(properties.get(key)) ? defaultValue : (String) properties.get(key);
     }
 
 }
