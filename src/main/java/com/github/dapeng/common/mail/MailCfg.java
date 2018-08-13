3a737d196d5b06cc3da2b29038e35b8830079a11
StruggleYang <yq1724555319@gmail.com>
2018-08-13 23:13:26 +0800

    环境变量支持

--- src/main/java/com/github/dapeng/common/mail/MailCfg.java
+++ src/main/java/com/github/dapeng/common/mail/MailCfg.java
@@ -13,6 +13,14 @@ import java.util.Properties;
 public class MailCfg {
 
     private static Properties properties;
+    private static String HOST_KEY = "mail.smtp.host";
+    private static String CHARSET_KEY = "mail.charset";
+    private static String AUTH_KEY = "mail.smtp.auth";
+    private static String USER_NAME_KEY = "mail.sender.username";
+    private static String PASSWORD_KEY = "mail.sender.password";
+    private static String NAME_KEY = "mail.from.name";
+    private static String TO_MAIL_KEY = "mail.to.email";
+
 
     static {
         properties = new Properties();
@@ -27,28 +35,43 @@ public class MailCfg {
     /**
      * 邮件服务器
      */
-    public final static String HOST = properties.getProperty("mail.smtp.host");
+    public final static String HOST = get(HOST_KEY, "");
 
     /**
      * 邮件编码
      */
-    public final static String CHARSET = properties.getProperty("mail.charset");
+    public final static String CHARSET = get(CHARSET_KEY, "");
 
     /**
      * 发送者邮箱
      */
-    public final static String DEFAULT_FROM_EMAIL = properties.getProperty("mail.sender.username");
+    public final static String DEFAULT_FROM_EMAIL = get(USER_NAME_KEY, "");
 
     /**
      * 发送者密码
      */
-    public final static String DEFAULT_FROM_PASSWD = properties.getProperty("mail.sender.password");
+    public final static String DEFAULT_FROM_PASSWD = get(PASSWORD_KEY, "");
 
     /**
      * 发送者名称
      */
-    public final static String DEFAULT_FROM_NAME = properties.getProperty("mail.from.name");
+    public final static String DEFAULT_FROM_NAME = get(NAME_KEY, "");
 
-    public final static String DEFAULT_TO_NAME = properties.getProperty("mail.to.email");
+    public final static String DEFAULT_TO_NAME = get(TO_MAIL_KEY, "");
+
+    public final static Boolean AUTH = Boolean.valueOf(get(AUTH_KEY, "true"));
+
+    public static String get(String key, String defaultValue) {
+        String envValue = System.getenv(key.replaceAll("\\.", "_"));
+
+        if (envValue == null)
+            return properties.getProperty(key, defaultValue);
+
+        return envValue;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MailCfg.DEFAULT_FROM_EMAIL);
+    }
 
 }
