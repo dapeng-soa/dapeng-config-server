ea1ac5b02a1dcd9f9aaad23147f828808d27fefd
StruggleYang <yq1724555319@gmail.com>
2018-08-23 14:52:31 +0800

    add compose stop_grace_period

--- src/main/java/com/github/dapeng/vo/DockerService.java
+++ src/main/java/com/github/dapeng/vo/DockerService.java
@@ -10,6 +10,7 @@ public class DockerService {
     private Map<String,String> environment;
     private List<String> ports;
     private String command;
+    private String stop_grace_period;
     private List<String> volumes;
     private List<String> labels;
     private List<String> extra_hosts;
@@ -79,6 +80,14 @@ public class DockerService {
 
     public List<String> getExtra_hosts() {
         return extra_hosts;
+    }
+
+    public String getStop_grace_period() {
+        return stop_grace_period;
+    }
+
+    public void setStop_grace_period(String stop_grace_period) {
+        this.stop_grace_period = stop_grace_period;
     }
 
     public void setExtra_hosts(List<String> extra_hosts) {
