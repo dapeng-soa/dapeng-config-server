b697033736cd81f5c3b44a911d0967b0ce89a774
StruggleYang <yq1724555319@gmail.com>
2018-09-06 17:20:06 +0800

    fix env by service on set

--- src/main/java/com/github/dapeng/dto/SetDto.java
+++ src/main/java/com/github/dapeng/dto/SetDto.java
@@ -1,5 +1,8 @@
 package com.github.dapeng.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author with struy.
  * Create by 2018/6/22 16:09
@@ -11,6 +14,7 @@ public class SetDto {
     private String name;
     private String env = "";
     private String remark = "";
+    private List<SubEnv>  subEnv = new ArrayList<>();
 
     public String getName() {
         return name;
@@ -37,4 +41,11 @@ public class SetDto {
         this.remark = remark;
     }
 
+    public List getSubEnv() {
+        return subEnv;
+    }
+
+    public void setSubEnv(List subEnv) {
+        this.subEnv = subEnv;
+    }
 }
