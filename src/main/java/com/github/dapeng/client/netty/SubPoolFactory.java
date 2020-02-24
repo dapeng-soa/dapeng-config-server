c7ae4ff17539ea7c17be32f99f36ca9799afaa51
StruggleYang <1724555319@qq.com>
2020-02-24 14:10:28 +0800

    fix version and agent_server_api

--- src/main/java/com/github/dapeng/client/netty/SubPoolFactory.java
+++ src/main/java/com/github/dapeng/client/netty/SubPoolFactory.java
@@ -20,7 +20,7 @@ public class SubPoolFactory {
     private final static ConcurrentHashMap<IpPort, SubPool> subPoolsMap = new ConcurrentHashMap(16);
     private static final ReentrantLock subPoolLock = new ReentrantLock();
 
-    public static SubPool getSubPool(String ip, int port)  {
+    public static SubPool getSubPool(String ip, int port) {
         IpPort ipPort = new IpPort(ip, port);
         SubPool subPool = subPoolsMap.get(ipPort);
         if (subPool == null) {
@@ -31,8 +31,8 @@ public class SubPoolFactory {
                     try {
                         subPool = new SubPool(ip, port);
                         subPoolsMap.put(ipPort, subPool);
-                    } catch (SoaException e) {
-                        CheckConnectInfo.set.add(ip+":"+port);
+                    } catch (Exception e) {
+                        CheckConnectInfo.set.add(ip + ":" + port);
                         LOGGER.error("创建SubPool连接异常");
                     }
                 }
